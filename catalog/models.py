f1d74654a1e09c859c134a2b5fcb1dd45c420950
nhuesman1043 <noah.huesman@gmail.com>
2024-03-26 09:05:53 -0500

    Stylized Main Content via Bootstrap

--- catalog/models.py
+++ catalog/models.py
@@ -42,7 +42,7 @@ class Book(models.Model):
         max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13,
                             unique=True,
-                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                            help_text='13 Character <a class="text-decoration-none text-link" href="https://www.isbn-international.org/content/what-isbn'
                                       '">ISBN number</a>')
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
