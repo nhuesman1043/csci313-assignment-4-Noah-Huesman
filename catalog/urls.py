4dabb8cbb18cbc9284e3734a860b0bf3b43435b5
nhuesman1043 <noah.huesman@gmail.com>
2024-04-03 18:04:27 -0500

    Made Project More Modular by Incorporating Generic Views for CRUD Operations & Final Touches

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,45 +5,40 @@ urlpatterns = [
     # Home
     path('', views.index, name='index'),
 
+    # Generic list view
+    path('<str:model_name>/', views.GenericListView.as_view(), name='generic-list'),
+
+    # Generic create view
+    path('<str:model_name>/create/', views.GenericCreateView.as_view(), name='create'),
+
+    # Generic update views - With int pk & uuid pk
+    path('<str:model_name>/<int:pk>/update/', views.GenericUpdateView.as_view(), name='update'),
+    path('<str:model_name>/<uuid:pk>/update/', views.GenericUpdateView.as_view(), name='update-uuid'),
+
+    # Generic delete views
+    path('<str:model_name>/<int:pk>/delete/', views.GenericDeleteView.as_view(), name='delete'),
+    path('<str:model_name>/<uuid:pk>/delete/', views.GenericDeleteView.as_view(), name='delete-uuid'),
+
+    # Detail views
     # Book
-    path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
-    path('book/create/', views.BookCreate.as_view(), name='book-create'),
-    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
-    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
 
     # Author
-    path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
-    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
-    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
-    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
    
     # Genre
-    path('genres/', views.GenreListView.as_view(), name='genres'),
     path('genre/<int:pk>', views.GenreDetailView.as_view(), name='genre-detail'),
-    path('genre/create/', views.GenreCreate.as_view(), name='genre-create'),
-    path('genre/<int:pk>/update/', views.GenreUpdate.as_view(), name='genre-update'),
-    path('genre/<int:pk>/delete/', views.GenreDelete.as_view(), name='genre-delete'),
 
     # Language
-    path('languages/', views.LanguageListView.as_view(), name='languages'),
     path('language/<int:pk>', views.LanguageDetailView.as_view(), name='language-detail'),
-    path('language/create/', views.LanguageCreate.as_view(), name='language-create'),
-    path('language/<int:pk>/update/', views.LanguageUpdate.as_view(), name='language-update'),
-    path('language/<int:pk>/delete/', views.LanguageDelete.as_view(), name='language-delete'),
 
     # BookInstance
-    path('bookinstances/', views.BookInstanceListView.as_view(), name='bookinstances'),
     path('bookinstance/<uuid:pk>', views.BookInstanceDetailView.as_view(), name='bookinstance-detail'),
-    path('bookinstance/create/', views.BookInstanceCreate.as_view(), name='bookinstance-create'),
-    path('bookinstance/<uuid:pk>/update/', views.BookInstanceUpdate.as_view(), name='bookinstance-update'),
-    path('bookinstance/<uuid:pk>/delete/', views.BookInstanceDelete.as_view(), name='bookinstance-delete'),
 
     # Renewal/borrow/return & user books
-    path('borrowed/', views.LoanedBooksListView.as_view(), name='borrowed'),
+    path('books/borrowed/', views.LoanedBooksListView.as_view(), name='borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
     path('book/<uuid:pk>/borrow/', views.book_borrow, name='borrow-book'),
     path('book/<uuid:pk>/return/', views.book_return_librarian, name='return-book-librarian'),
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('books/mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -9,10 +9,11 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.apps import apps
 
 # Home
 def index(request):
-    """View function for home page of site."""
+    '''View function for home page of site.'''
 
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
@@ -45,68 +46,22 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-# List views
-# Book list
-class BookListView(generic.ListView):
-    model = Book
+# List view
+class GenericListView(generic.ListView):
+    template_name = 'list_generic.html'
+    context_object_name = 'object_list'
     paginate_by = 10
 
-# Author list
-class AuthorListView(generic.ListView):
-    model = Author
-    paginate_by = 10
-
-# Genre list
-class GenreListView(generic.ListView):
-    model = Genre
-    paginate_by = 10
+    def get_queryset(self):
+        model_name = self.kwargs['model_name']
+        model = apps.get_model('catalog', model_name)
+        return model.objects.all()
     
-# Language list
-class LanguageListView(generic.ListView):
-    model = Language
-    paginate_by = 10
-
-class LanguageDetailView(generic.DetailView):
-    model = Language
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        language = self.object
-        context['books'] = language.book_set.all() 
+        context['model_name'] = self.kwargs['model_name']
         return context
     
-# Book Instance list
-class BookInstanceListView(generic.ListView):
-    model = BookInstance
-    paginate_by = 20
-
-# List showing loaned books
-class LoanedBooksListView(LoginRequiredMixin,generic.ListView):
-    """Generic class-based view listing books on loan."""
-    model = BookInstance
-    template_name = 'catalog/bookinstance_borrowed_list.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        return (
-            BookInstance.objects.filter(status__exact='o')
-            .order_by('due_back')
-        )
-
-# List showing a user's borrowed books
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
-    """Generic class-based view listing books on loan to current user."""
-    model = BookInstance
-    template_name = 'catalog/bookinstance_list_borrowed_user.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        return (
-            BookInstance.objects.filter(borrower=self.request.user)
-            .filter(status__exact='o')
-            .order_by('due_back')
-        )
-
 # Detail views
 # Book details
 class BookDetailView(generic.DetailView):
@@ -152,136 +107,163 @@ class BookInstanceDetailView(generic.DetailView):
         context['books'] = bookinstance
         return context
 
-# CRUD views
-# Book
-class BookCreate(PermissionRequiredMixin, CreateView):
-    model = Book
-    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
-    permission_required = 'catalog.add_book'
+# Create view
+class GenericCreateView(PermissionRequiredMixin, CreateView):
+    # Gets model from kwargs
+    def get_model(self):
+        # Retrieve the model_name from URL parameters
+        model_name = self.kwargs['model_name']
 
-class BookUpdate(PermissionRequiredMixin, UpdateView):
-    model = Book
-    # Not recommended (potential security issue if more fields added)
-    fields = '__all__'
-    permission_required = 'catalog.change_book'
+        # Fetch the actual model class based on the model_name
+        return apps.get_model('catalog', model_name)
+    
+    # Get fields based on dictionary 
+    def get_fields(self):
+        # Dictionary to map model_name to fields - default is all
+        fields_map = {
+            'Book': ['title', 'author', 'summary', 'isbn', 'genre', 'language'],
+            'Author': ['first_name', 'last_name', 'date_of_birth', 'date_of_death'],  
+            'Genre': ['name'],
+            'Language': ['name'],
+            'BookInstance' : ['book', 'imprint', 'due_back', 'borrower', 'status'], 
+        }
 
-class BookDelete(PermissionRequiredMixin, DeleteView):
-    model = Book
-    success_url = reverse_lazy('books')
-    permission_required = 'catalog.delete_book'
+        # Fetch fields based on model_name from the dictionary
+        model_name = self.kwargs['model_name']
+        return fields_map.get(model_name, '__all__') 
+    
+    # Check if user has required permissions based on model_name
+    def get_permission_required(self):
+        model_name = self.kwargs['model_name']
+        return ['catalog.add_' + model_name.lower()]
+      
+    # Redirect to a success URL after creating the object
+    def get_success_url(self):
+        # Go back to generic-list with model_name passed in
+        return reverse('generic-list', kwargs={'model_name': self.kwargs['model_name']})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_name'] = self.kwargs['model_name']
+        return context
+    
+    # Pass in 
+    template_name = 'form_generic.html'
+    model = property(get_model)
+    fields = property(get_fields)
+    permission_required = property(get_permission_required)
+    success_url = property(get_success_url)
 
+# Update view
+class GenericUpdateView(PermissionRequiredMixin, UpdateView):
+    # Gets model from kwargs
+    def get_model(self):
+        # Retrieve the model_name from URL parameters
+        model_name = self.kwargs['model_name']
+
+        # Fetch the actual model class based on the model_name
+        return apps.get_model('catalog', model_name)
+    
+    # Get fields based on dictionary 
+    def get_fields(self):
+        # Dictionary to map model_name to fields - default is all
+        fields_map = {
+            'BookInstance' : ['book', 'imprint', 'due_back', 'borrower', 'status'], 
+        }
+
+        # Fetch fields based on model_name from the dictionary
+        model_name = self.kwargs['model_name']
+        return fields_map.get(model_name, '__all__') 
+    
+    # Check if user has required permissions based on model_name
+    def get_permission_required(self):
+        model_name = self.kwargs['model_name']
+        return ['catalog.change_' + model_name.lower()]
+      
+    # Redirect to a success URL after creating the object
+    def get_success_url(self):
+        # Go back to generic-list with model_name passed in
+        return reverse('generic-list', kwargs={'model_name': self.kwargs['model_name']})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_name'] = self.kwargs['model_name']
+        return context
+    
+    # Pass in 
+    template_name = 'form_generic.html'
+    model = property(get_model)
+    fields = property(get_fields)
+    permission_required = property(get_permission_required)
+    success_url = property(get_success_url)
+
+# Delete view
+class GenericDeleteView(PermissionRequiredMixin, DeleteView):
+    # Gets model from kwargs
+    def get_model(self):
+        # Retrieve the model_name from URL parameters
+        model_name = self.kwargs['model_name']
+
+        # Fetch the actual model class based on the model_name
+        return apps.get_model('catalog', model_name)
+    
+    # Check if user has required permissions based on model_name
+    def get_permission_required(self):
+        model_name = self.kwargs['model_name']
+        return ['catalog.delete_' + model_name.lower()]
+      
+    # Redirect to a success URL after creating the object
+    def get_success_url(self):
+        # Go back to generic-list with model_name passed in
+        return reverse('generic-list', kwargs={'model_name': self.kwargs['model_name']})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_name'] = self.kwargs['model_name']
+        return context
+    
     def form_valid(self, form):
         try:
             self.object.delete()
             return HttpResponseRedirect(self.success_url)
         except Exception as e:
             return HttpResponseRedirect(
-                reverse("book-delete", kwargs={"pk": self.object.pk})
+                reverse('book-delete', kwargs={'pk': self.object.pk})
             )
+    
+    # Pass in 
+    template_name = 'confirm_delete_generic.html'
+    model = property(get_model)
+    permission_required = property(get_permission_required)
+    success_url = property(get_success_url)
 
-# Author
-class AuthorCreate(PermissionRequiredMixin, CreateView):
-    model = Author
-    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
-    permission_required = 'catalog.add_author'
 
-class AuthorUpdate(PermissionRequiredMixin, UpdateView):
-    model = Author
-    # Not recommended (potential security issue if more fields added)
-    fields = '__all__'
-    permission_required = 'catalog.change_author'
-
-class AuthorDelete(PermissionRequiredMixin, DeleteView):
-    model = Author
-    success_url = reverse_lazy('authors')
-    permission_required = 'catalog.delete_author'
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-            return HttpResponseRedirect(self.success_url)
-        except Exception as e:
-            return HttpResponseRedirect(
-                reverse("author-delete", kwargs={"pk": self.object.pk})
-            )
-
-# Genre
-class GenreCreate(PermissionRequiredMixin, CreateView):
-    model = Genre
-    fields = ['name']
-    permission_required = 'catalog.add_genre'
-
-class GenreUpdate(PermissionRequiredMixin, UpdateView):
-    model = Genre
-    # Not recommended (potential security issue if more fields added)
-    fields = '__all__'
-    permission_required = 'catalog.change_genre'
-
-class GenreDelete(PermissionRequiredMixin, DeleteView):
-    model = Genre
-    success_url = reverse_lazy('genres')
-    permission_required = 'catalog.delete_genre'
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-            return HttpResponseRedirect(self.success_url)
-        except Exception as e:
-            return HttpResponseRedirect(
-                reverse("genre-delete", kwargs={"pk": self.object.pk})
-            )
-
-# Language
-class LanguageCreate(PermissionRequiredMixin, CreateView):
-    model = Language
-    fields = ['name']
-    permission_required = 'catalog.add_language'
-
-class LanguageUpdate(PermissionRequiredMixin, UpdateView):
-    model = Language
-    # Not recommended (potential security issue if more fields added)
-    fields = '__all__'
-    permission_required = 'catalog.change_language'
-
-class LanguageDelete(PermissionRequiredMixin, DeleteView):
-    model = Language
-    success_url = reverse_lazy('languages')
-    permission_required = 'catalog.delete_language'
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-            return HttpResponseRedirect(self.success_url)
-        except Exception as e:
-            return HttpResponseRedirect(
-                reverse("language-delete", kwargs={"pk": self.object.pk})
-            )
-
-# Book Instance
-class BookInstanceCreate(PermissionRequiredMixin, CreateView):
+# List showing loaned books
+class LoanedBooksListView(LoginRequiredMixin,generic.ListView):
+    '''Generic class-based view listing books on loan.'''
     model = BookInstance
-    fields = ['book', 'imprint', 'due_back', 'borrower', 'status']
-    permission_required = 'catalog.add_bookinstance'
+    template_name = 'catalog/bookinstance_borrowed_list.html'
+    paginate_by = 10
 
-class BookInstanceUpdate(PermissionRequiredMixin, UpdateView):
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(status__exact='o')
+            .order_by('due_back')
+        )
+
+# List showing a user's borrowed books
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    '''Generic class-based view listing books on loan to current user.'''
     model = BookInstance
-    # Not recommended (potential security issue if more fields added)
-    fields = ['book', 'imprint', 'due_back', 'borrower', 'status']
-    permission_required = 'catalog.change_bookinstance'
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
 
-class BookInstanceDelete(PermissionRequiredMixin, DeleteView):
-    model = BookInstance
-    success_url = reverse_lazy('bookinstances')
-    permission_required = 'catalog.delete_bookinstance'
-
-    def form_valid(self, form):
-        try:
-            self.object.delete()
-            return HttpResponseRedirect(self.success_url)
-        except Exception as e:
-            return HttpResponseRedirect(
-                reverse("bookinstance-delete", kwargs={"pk": self.object.pk})
-            )
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
 
 # Book Instance renewal/borrow/return
 # Allow librarian to renew loaned and overdue books
